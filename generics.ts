69e8f89d54446b80a3c0641888a4a45978a93388
TJ-Rocke <spook.devv@gmail.com>
2025-05-17 00:33:46 -0400

    Understanding Generics

---
+++ generics.ts
@@ -0,0 +1,8 @@
+// let names: string[]= ["Tony", "Anthony"]; // one way
+let names: Array<String> = ["Tony", "Anthony"]; // another valid way / generic way
+
+/* whats special about Generics?
+ * Types where multiple types need to work together with other types in order to accurately describe a certain value type
+ * Two types working together (e.g. Array and string working together ^)
+ * The poiint behing Generic types, Create flexible types where multiple types work together
+ */
