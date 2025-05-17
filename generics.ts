8d764095b1900a1d9691ba3e02f4efd2572b1ef4
TJ-Rocke <spook.devv@gmail.com>
2025-05-17 00:40:47 -0400

    Creating and Using a Generic type

--- generics.ts
+++ generics.ts
@@ -6,3 +6,13 @@ let names: Array<String> = ["Tony", "Anthony"]; // another valid way / generic w
  * Two types working together (e.g. Array and string working together ^)
  * The poiint behing Generic types, Create flexible types where multiple types work together
  */
+
+type DataStore<placeholderType> = {
+  // can accept multiple placeholder types
+  // [key: string]: string | number;
+  [key: string]: placeholderType;
+}; // benefit is a very flexible DataStore type which can be used on as many stores as we want. When its created I can decide what kind of data value can be stored in it
+
+let store: DataStore<string | boolean> = {};
+store.name = "Tony";
+store.isCoder = true;
